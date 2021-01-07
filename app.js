fdb144d0342be468827e0ad13e39a1c8a5b0730c
Merkan <merkan@mail.com>
2021-01-07 15:42:58 +0100

    nodejs client server

---
+++ app.js
@@ -0,0 +1,15 @@
+var http = require('http'); 
+var fs = require('fs');
+
+var server = http.createServer(function(req, res) {
+ fs.readFile('index.html', function (err, data) {
+         res.write(data);
+         res.end('mesaj bitti');
+
+     });
+});
+
+server.listen(8000);
+
+
+
